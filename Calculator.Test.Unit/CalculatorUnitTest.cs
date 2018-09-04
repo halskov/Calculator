37c8af635e2d39ad6cc4b16004a352c20ef9fb17
Jakob Halskov <jakobhalskov@gmail.com>
2018-09-04 10:45:20 +0200

    Added three tests for subtraction

--- Calculator.Test.Unit/CalculatorUnitTest.cs
+++ Calculator.Test.Unit/CalculatorUnitTest.cs
@@ -10,6 +10,9 @@ namespace Calculator.Test.Unit
     [TestFixture]
     public class CalculatorUnitTest
     {
+        /*
+         * Addition
+         */
         [TestCase(2, 4, 6)]
         [TestCase(-10, 6, -4)]
         [TestCase(-10, -12, -22)]
@@ -17,6 +20,18 @@ namespace Calculator.Test.Unit
         {
             var uut = new Calculator();
             Assert.That(uut.Add(a, b), Is.EqualTo(result));
+        }
+
+        /*
+         * Subtraction
+         */
+        [TestCase(2, 4, -2)]
+        [TestCase(10, 1, 9)]
+        [TestCase(-10, -12, 2)]
+        public void Subtract_Test(double a, double b, double result)
+        {
+            var uut = new Calculator();
+            Assert.That(uut.Subtract(a, b), Is.EqualTo(result));
         }
 
         [TestCase(2, 2)]
